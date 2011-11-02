04b9068de52b3cefe412983cf24825a6b8868d1f
Tatsuya Fukata <tatsuya.fukata@gmail.com>
2011-11-02 22:05:18 +0900

    helloworld

--- src/heroku_helloworld/core.clj
+++ src/heroku_helloworld/core.clj
@@ -1,1 +1,11 @@
-(ns heroku_helloworld.core)
+(ns heroku_helloworld.core
+  (:use ring.adapter.jetty))
+
+(defn app [req]
+  {:status 200
+   :headers {"Content-Type" "text/html"}
+   :body "Hello World"})
+
+(defn -main []
+  (let [port (Integer/parseInt (get (System/getenv) "PORT" "8080"))]
+    (run-jetty app {:port port})))
