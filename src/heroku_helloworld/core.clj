3cc968fc09d5819233ab39d561a5f851cb296765
Tatsuya Fukata <tatsuya.fukata@gmail.com>
2011-11-02 21:13:46 +0900

    first

---
+++ src/heroku_helloworld/core.clj
@@ -0,0 +1,1 @@
+(ns heroku_helloworld.core)
---
+++ test/heroku_helloworld/test/core.clj
@@ -0,0 +1,6 @@
+(ns heroku_helloworld.test.core
+  (:use [heroku_helloworld.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
