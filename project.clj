0abdcd069ceb66dd2a38139d7e9ebbfeca91ffde
Tatsuya Fukata <tatsuya.fukata@gmail.com>
2011-11-03 21:33:02 +0900

    compojure

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject heroku_helloworld "1.0.0-SNAPSHOT"
   :description "Clojure on Heroku"
   :dependencies [[org.clojure/clojure "1.2.1"]
+                 [compojure "0.6.5"]
                  [ring/ring-jetty-adapter "0.3.9"]])
--- src/heroku_helloworld/core.clj
+++ src/heroku_helloworld/core.clj
@@ -1,10 +1,9 @@
 (ns heroku_helloworld.core
-  (:use ring.adapter.jetty))
+  (:use compojure.core
+        ring.adapter.jetty))
 
-(defn app [req]
-  {:status 200
-   :headers {"Content-Type" "text/html"}
-   :body "Hello World"})
+(defroutes app 
+  (GET "/" {} "Hello World"))
 
 (defn -main []
   (let [port (Integer/parseInt (get (System/getenv) "PORT" "8080"))]
